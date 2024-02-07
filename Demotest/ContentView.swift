716698fc821e1d6c56edff511db1f42032e5cdf4
Jasmine <jazzymberry@gmail.com>
2024-02-07 09:37:23 -0600

    Xcode project Update.

--- Demotest/ContentView.swift
+++ Demotest/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
+            Text("Project Updated!")
             List {
                 ForEach(items) { item in
                     NavigationLink {
